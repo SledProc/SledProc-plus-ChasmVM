b348636d80e9469e09bf43212b5c962d9076722b
SledProc <nlevisrael@aol.com>
2025-03-03 21:41:10 -0500

    update

--- code/cpp/src/prelim/contrib/all/all/sk/rz/rz-sled/rz-graph-asg-console/main.cpp
+++ code/cpp/src/prelim/contrib/all/all/sk/rz/rz-sled/rz-graph-asg-console/main.cpp
@@ -129,6 +129,7 @@ struct Page_Ref_Pair;
 struct Page_Ref {
  ushort number;
  uint note;
+ uint secondary_note;
  QString roman;
 
  QString to_code() const;
@@ -144,6 +145,14 @@ struct Page_Ref {
 };
 
 struct Page_Ref_Pair : QPair<Page_Ref, Page_Ref> {
+
+ static Page_Ref_Pair default_values()
+ {
+  Page_Ref_Pair result;
+  result.first = {0, 0, 0};
+  result.second = {0, 0, 0};
+  return result;
+ }
 
  static Page_Ref_Pair from_vector(const QVector<Page_Ref>& vec)
  {
@@ -182,14 +191,10 @@ QString Page_Ref::to_code() const
 {
  if(roman.isEmpty())
  {
+  if(secondary_note)
+    return "@%1/%2;%3"_qt.arg(number).arg(note).arg(secondary_note);
   if(note)
-  {
-   QString n = QString::number(note);
-   int ix = n.lastIndexOf("000");
-   if(ix != -1)
-     n.replace(ix, 3, ";");
-   return "@%1/%2"_qt.arg(number).arg(n);
-  }
+    return "@%1/%2"_qt.arg(number).arg(note);
   return "%%1"_qt.arg(number);
  }
  return "$%1"_qt.arg(roman);
@@ -198,7 +203,8 @@ QString Page_Ref::to_code() const
 
 Page_Ref Page_Ref::_from(QString qs)
 {
- ushort number = 0, note = 0;
+ ushort number = 0;
+ uint note = 0, secondary_note = 0;
  QString roman;
 
  QRegularExpression qre ("^(\\d+)n(\\d+)");
@@ -206,6 +212,7 @@ Page_Ref Page_Ref::_from(QString qs)
  if(match.hasMatch())
  {
   number = match.captured(1).toInt();
+  QString n = match.captured(2);
   note = match.captured(2).toInt();
  }
  else
@@ -218,7 +225,18 @@ Page_Ref Page_Ref::_from(QString qs)
     number = qs.toInt();
  }
 
- return {number, note, roman};
+ if(note > 1000)
+ {
+  QString n = QString::number(note);
+  int ix = n.lastIndexOf("000");
+  if(ix != -1)
+  {
+   note = n.mid(0, ix).toInt();
+   secondary_note = n.mid(ix + 3).toInt();
+  }
+ }
+
+ return {number, note, secondary_note, roman};
 }
 
 Page_Ref_Pair Page_Ref::from(QString qs)
@@ -240,6 +258,7 @@ struct Index_Entry {
 
  Index_Entry* parent;
  int count_in_parent;
+ int id;
  QString key;
  QVector<Page_Ref_Pair> refs;
  QString carried;
@@ -264,11 +283,11 @@ void Index_Entry::add_redirect(QString text, QString supplement, QVector<Index_E
   QString key = text.mid(0, ix);
   QString carried = text.mid(ix + 1).simplified();
 
-  entries.push_back({nullptr, 0, key, {}, carried, 0});
+  entries.push_back({nullptr, 0, 0, key, {}, carried, 0});
  }
  else
  {
-  entries.push_back({nullptr, 0, text, {}, {}, 0});
+  entries.push_back({nullptr, 0, 0, text, {}, {}, 0});
  }
 }
 
@@ -276,6 +295,9 @@ void write_index_entries(const QVector<Index_Entry>& index_entries, QTextStream&
 {
  for(const Index_Entry& ie : index_entries)
  {
+  if(ie.key.contains("Childhelp"))
+    CAON_DEBUG_NOOP
+
   qts << "\n ";
 
   if(ie.parent)
@@ -283,7 +305,7 @@ void write_index_entries(const QVector<Index_Entry>& index_entries, QTextStream&
   else
     qts << "[[ \"" << ie.key << "\"";
 
-
+  qts << " &" << ie.id;
 
   if(ie.parent)
     qts << " *" << ie.count_in_parent;
@@ -311,11 +333,200 @@ void write_index_entries(const QVector<Index_Entry>& index_entries, QTextStream&
   qts << "\n ]] \n";
 
  }
+}
+
+void parse_ref(QString text, Page_Ref& ref)
+{
+ if(text.startsWith("%"))
+   ref.number = text.mid(1).toInt();
+ else if(text.startsWith("@"))
+ {
+  QRegularExpression qre("(\\d+)/(\\d+)(?:;(\\d+))?");
+  QRegularExpressionMatch match = qre.match(text);
+  if(match.hasMatch())
+  {
+   ref.number = match.captured(1).toInt();
+   ref.note = match.captured(2).toInt();
+
+   if(!match.captured(3).isEmpty())
+     ref.secondary_note = match.captured(3).toInt();
+  }
+ }
+ else if(text.startsWith("$"))
+ {
+  ref.roman = text.mid(1);
+ }
+}
+
+void parse_ref_pair(QString text, Page_Ref_Pair& pr)
+{
+ if(text.startsWith("^^"))
+ {
+  text = text.mid(2);
+  QStringList qsl = text.split("--");
+  parse_ref(qsl.value(0), pr.first);
+  parse_ref(qsl.value(1), pr.second);
+  CAON_DEBUG_NOOP
+ }
+ else if(text.startsWith("^"))
+ {
+  parse_ref(text.mid(1), pr.first);
+  CAON_DEBUG_NOOP
+ }
+
+}
+
+void read_index_entry(QString text, Index_Entry& ie)
+{
+ int qspos = text.indexOf("\"") + 1;
+ int qepos = text.indexOf("\"", qspos);
+
+ QString key = text.mid(qspos, qepos - qspos);
+
+ text = text.mid(qepos + 1).simplified();
+
+ // count_in_parent, sub_count, carried, sub_carried
+
+ QString id_token;
+ QRegularExpression id_token_regex("^\\s*&(\\d+)");
+ QRegularExpressionMatch id_token_regex_match = id_token_regex.match(text);
+ if(id_token_regex_match.hasMatch())
+ {
+  id_token = id_token_regex_match.captured(1);
+  text = text.mid(id_token_regex_match.capturedEnd());
+ }
+
+ QString count_in_parent_token;
+ QRegularExpression count_in_parent_token_regex("^\\s*\\*(\\d+)");
+ QRegularExpressionMatch count_in_parent_token_regex_match = count_in_parent_token_regex.match(text);
+ if(count_in_parent_token_regex_match.hasMatch())
+ {
+  count_in_parent_token = count_in_parent_token_regex_match.captured(1);
+  text = text.mid(count_in_parent_token_regex_match.capturedEnd());
+ }
+
+ QString sub_count_token;
+ QRegularExpression sub_count_token_regex("^\\s*\\#(\\d+)");
+ QRegularExpressionMatch sub_count_token_regex_match = sub_count_token_regex.match(text);
+ if(sub_count_token_regex_match.hasMatch())
+ {
+  sub_count_token = sub_count_token_regex_match.captured(1);
+  text = text.mid(sub_count_token_regex_match.capturedEnd());
+ }
+
+
+
+ QString carried;
+ QRegularExpression carried_regex("^\\s*\\+\\{([^}]*)\\}");
+ QRegularExpressionMatch carried_regex_match = carried_regex.match(text);
+ if(carried_regex_match.hasMatch())
+ {
+  carried = carried_regex_match.captured(1);
+  text = text.mid(carried_regex_match.capturedEnd());
+ }
+
+ QString sub_carried;
+ QRegularExpression sub_carried_regex("^\\s*-\\{([^}]*)\\}");
+ QRegularExpressionMatch sub_carried_regex_match = sub_carried_regex.match(text);
+ if(sub_carried_regex_match.hasMatch())
+ {
+  sub_carried = sub_carried_regex_match.captured(1);
+  text = text.mid(sub_carried_regex_match.capturedEnd());
+ }
+
+ ie.carried = carried;
+ ie.sub_carried = sub_carried;
+ ie.key = key;
+ ie.count_in_parent = count_in_parent_token.toInt();
+ ie.sub_count = sub_count_token.toInt();
+ ie.id = id_token.toInt();
+
+ if(ie.key.contains("Childhelp"))
+   CAON_DEBUG_NOOP
+
+
+ QRegularExpression follow_regex("^\\s*=>\\s*");
+ QRegularExpressionMatch follow_regex_match = follow_regex.match(text);
+ if(follow_regex_match.hasMatch())
+ {
+  text = text.mid(follow_regex_match.capturedEnd());
+ }
+ else
+   return;
+
+ text = text.simplified();
+ if(text.isEmpty())
+    return;
+
+ QStringList qsl = text.split(" -> ");
+
+ for(QString ref : qsl)
+ {
+  Page_Ref_Pair pr = Page_Ref_Pair::default_values();// {{0, 0, QString{}}, {0, 0, QString{}}};
+  parse_ref_pair(ref, pr);
+  ie.refs.push_back(pr);
+ }
+
+// if(text.startsWith(QRegularExpression))
+
+
+ CAON_DEBUG_NOOP
 
 }
 
 
+void read_index_entries(QString text, QVector<Index_Entry>& ies)
+{
+ int spos = 0, epos = 0;
+
+ Index_Entry* current_parent = nullptr;
+
+ while(true)
+ {
+  spos = text.indexOf("[[", epos);
+  if(spos == -1)
+    break;
+
+  epos = text.indexOf("]]", spos);
+  if(epos == -1)
+    break;
+
+  Index_Entry ie;
+
+  read_index_entry(text.mid(spos + 2, epos - spos - 2).simplified(), ie);
+  ies.push_back(ie);
+
+  if(ie.sub_count)
+    current_parent = &ies.back();
+  else if(ie.count_in_parent)
+    ies.back().parent = current_parent;
+  else
+    ies.back().parent = nullptr;
+ }
+}
+
+
 int main(int argc, char *argv[])
+{
+ QString ifile = "/home/nlevisrael/Downloads/m2m/dindex.txt";
+ QString ofile = "/home/nlevisrael/Downloads/m2m/tindex.txt";
+
+ QString text = KA::TextIO::load_file(ifile);
+
+ QVector<Index_Entry> index_entries;
+
+ read_index_entries(text, index_entries);
+
+ QString otext;
+ QTextStream oqts(&otext);
+
+ write_index_entries(index_entries, oqts);
+ KA::TextIO::save_file(ofile, otext);
+
+
+}
+
+int main3(int argc, char *argv[])
 {
  QString file = "/home/nlevisrael/Downloads/m2m/index.txt";
  QString ofile = "/home/nlevisrael/Downloads/m2m/oindex.txt";
@@ -394,17 +605,8 @@ int main(int argc, char *argv[])
   {
    ++_lc;
 
-   if(line == "Anderson% Karen")
-     CAON_DEBUG_NOOP;
-
-   if(line.contains("Leon"))
-     CAON_DEBUG_NOOP;
-
-   if(line.contains("Bridget"))
-     CAON_DEBUG_NOOP;
-
-   if(line.contains("Billings"))
-     CAON_DEBUG_NOOP;
+//   if(line.contains("Billings"))
+//     CAON_DEBUG_NOOP;
 
    if(line.endsWith("//"))
    {
@@ -488,7 +690,7 @@ int main(int argc, char *argv[])
 
    if(!current_key.isEmpty())
    {
-    index_entries.push_back({nullptr, 0, current_key, {}, carried_see, 0});
+    index_entries.push_back({nullptr, 0, 0, current_key, {}, carried_see, 0});
     current_key.clear();
     //?carried_see.clear();
    }
@@ -548,6 +750,7 @@ int main(int argc, char *argv[])
 
  Index_Entry* ie_for_sub_increment;
 
+ int count_adj = 0;
  for(Index_Entry& ie : index_entries)
  {
   if(ie.parent && ie.parent == ie_for_sub_increment)
@@ -555,6 +758,9 @@ int main(int argc, char *argv[])
 
   QString key = ie.key;
   key.replace("%", ",");
+
+  ie.carried.replace("%", ",");
+  ie.sub_carried.replace("%", ",");
 
   if(ie.sub_count && ie.key.contains(":"))
   {
@@ -569,12 +775,14 @@ int main(int argc, char *argv[])
    //ie1.refs =
    ie1.sub_carried = ie.sub_carried;
    ie1.sub_count = ie.sub_count;
+   ie1.id = ++count_adj;
 
    ie2.count_in_parent = 1; // should be 0
    ie2.key = split.value(1).simplified();
    ie2.parent = &ie; // should be nullptr
    ie2.refs = ie.refs;
    ie2.sub_count = 0;
+   ie2.id = ++count_adj;
 
    index_entries_adj.push_back(ie1);
    index_entries_adj.push_back(ie2);
@@ -582,6 +790,7 @@ int main(int argc, char *argv[])
   else
   {
    ie.key = key;
+   ie.id = ++count_adj;
    index_entries_adj.push_back(ie);
   }
  }
