e2ebf40cadf0cfb0782bfbc1227d58563b69d8da
SledProc <nlevisrael@aol.com>
2025-11-04 15:34:58 -0500

    update

--- code/cpp/src/prelim/contrib/all/all/apk/gtagml/gtagml/gtagml-console/main.cpp
+++ code/cpp/src/prelim/contrib/all/all/apk/gtagml/gtagml/gtagml-console/main.cpp
@@ -251,10 +251,10 @@ int main(int argc, char *argv[])
 
 // _main(file, folder, manfolder);
 
-// file = "/home/nlevisrael/gits/sled/s+p-prelim/ar/m2m/ch4/ch4.gt";
-// folder = "/home/nlevisrael/gits/sled/s+p-prelim/ar/m2m/ch4";
+ file = "/home/nlevisrael/gits/sled/s+p-prelim/ar/m2m/ch4/ch4.gt";
+ folder = "/home/nlevisrael/gits/sled/s+p-prelim/ar/m2m/ch4";
 
-// _main(file, folder, manfolder);
+ _main(file, folder, manfolder);
 
 // file = "/home/nlevisrael/gits/sled/s+p-prelim/ar/m2m/ch5/ch5.gt";
 // folder = "/home/nlevisrael/gits/sled/s+p-prelim/ar/m2m/ch5";
@@ -291,10 +291,10 @@ int main(int argc, char *argv[])
 
 // _main(file, folder, manfolder);
 
- file = "/home/nlevisrael/gits/sled/s+p-prelim/ar/m2m/notes/notes.gt";
- folder = "/home/nlevisrael/gits/sled/s+p-prelim/ar/m2m/notes";
+// file = "/home/nlevisrael/gits/sled/s+p-prelim/ar/m2m/notes/notes.gt";
+// folder = "/home/nlevisrael/gits/sled/s+p-prelim/ar/m2m/notes";
 
- _main(file, folder, manfolder);
+// _main(file, folder, manfolder);
 
 // file = "/home/nlevisrael/gits/sled/s+p-prelim/ar/m2m/references/references.gt";
 // folder = "/home/nlevisrael/gits/sled/s+p-prelim/ar/m2m/references";
