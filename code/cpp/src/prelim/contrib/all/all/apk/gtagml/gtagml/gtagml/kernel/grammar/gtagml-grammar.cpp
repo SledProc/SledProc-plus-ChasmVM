dd69466756144551532e1e4a037d407ae4c4513c
SledProc <nlevisrael@aol.com>
2026-01-26 01:00:14 -0500

    update

--- code/cpp/src/prelim/contrib/all/all/apk/gtagml/gtagml/gtagml/kernel/grammar/gtagml-grammar.cpp
+++ code/cpp/src/prelim/contrib/all/all/apk/gtagml/gtagml/gtagml/kernel/grammar/gtagml-grammar.cpp
@@ -576,6 +576,22 @@ void GTagML_Grammar::init(GTagML_Parser& p, GTagML_Graph& g, GTagML_Graph_Build&
 
  add_rule( flags_all_(parse_context ,emph_sample_mode),
    gtagml_context, "leave-sample-mode",
+   " / ; (?! ;) "
+   ,[&]
+ {
+  graph_build.leave_sample_mode();
+ });
+
+ add_rule( flags_all_(parse_context ,emph_sample_mode),
+   gtagml_context, "leave-sample-mode",
+   " / (?! \\w) "
+   ,[&]
+ {
+  graph_build.leave_sample_mode();
+ });
+
+ add_rule( flags_all_(parse_context ,emph_sample_mode),
+   gtagml_context, "leave-sample-mode",
    " / (?! \\w) "
    ,[&]
  {
